86ea557b7a0c0976c3a466fb5c64f8ddaed88e94
Your Name <you@example.com>
2023-08-02 14:18:42 +0500

    Initial commit

---
+++ src/main/java/com/example/smartmobilepaymentservice/SmartMobilePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartmobilepaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMobilePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMobilePaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartmobilepaymentservice/SmartMobilePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartmobilepaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMobilePaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
